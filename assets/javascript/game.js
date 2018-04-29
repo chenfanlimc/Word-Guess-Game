d7e800619559250d1de56de66e97ab4e97a47ce6
cli <cli@mastercontrol.com>
2018-04-29 16:23:56 -0600

    working version of homework, now I just need to link to portfolio and possibly add some html/css changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -24,14 +24,14 @@ function createWordLines() {
 function resetGame() {
     guessesRemaining = 5;
     lettersAlreadyGuessed = [];
-    randomizeWord(wordArray);
+    wordToGuess = randomizeWord(wordArray);
     createWordLines();
     document.getElementById("hangManGuesses").innerHTML = wordLines.join(" ");
     var headerText =
         "<p>Wins: " + wins + "</p>" +
         "<p>Losses: " + losses + "</p>" +
         "Press Any Key to Begin";
-    document.getElementById("hangMan").innerHTML = headerText
+    document.getElementById("hangMan").innerHTML = headerText;
 }
 
 
@@ -51,9 +51,10 @@ document.onkeyup = function (guess) {
 
     //First, make sure user pressed a valid letter for input before running block, else do an alert; ignore shift
     if (alphabetArray.includes(userGuess)) {
-        //make sure the guess is not something already guessed, if it is, function returns nothing
+        document.getElementById("showAlert").innerHTML = "";
+        //make sure the guess is not something already guessed, if it is, show said warning to player, then do nothing else
         if (lettersAlreadyGuessed.includes(userGuess)) {
-            alert("You already guessed that character!");
+            document.getElementById("showAlert").innerHTML = "You already guessed that character!";
             return;
         }
         //check if the userGuess matches one or more of the characters in the wordToGuess
@@ -64,31 +65,39 @@ document.onkeyup = function (guess) {
                 charMatches++;
             }
         }
+        //if there are no positive matches for character guessed, guessesRemaining drops by one
         if (charMatches === 0) {
             guessesRemaining--;
         }
+
+        //add the userGuess to the letters already guessed list
         lettersAlreadyGuessed.push(userGuess);
 
+    //if user decides to use shift key to guess a capital letter, do not prompt for valid letter
     } else if (guess.keyCode == "16") {
         //DO NOTHING
     } else {
-        alert("Please enter a valid letter");
+        //prompt warning to player to enter valid letter
+        document.getElementById("showAlert").innerHTML = "Please enter a valid letter";
     }
 
+    //based on if valid entry or not, update headerText variable with proper information
     if (alphabetArray.includes(userGuess)) {
         var headerText =
             "<p>You chose the letter " + userGuess + ". </p>" +
-            "<p>You have " + guessesRemaining + " remaining. </p>" +
+            "<p>You have " + guessesRemaining + " guesses remaining. </p>" +
             "<p>You have guessed the following letters: " + lettersAlreadyGuessed + "</p>" +
             "<p>Wins: " + wins + "</p>" +
             "<p>Losses: " + losses + "</p>"
     } else {
         var headerText =
-            "<p>You have " + guessesRemaining + " remaining. </p>" +
+            "<p>You have " + guessesRemaining + " guesses remaining. </p>" +
             "<p>You have guessed the following letters: " + lettersAlreadyGuessed + "</p>" +
             "<p>Wins: " + wins + "</p>" +
             "<p>Losses: " + losses + "</p>"
     }
+    //update headerText html element
+    document.getElementById("hangMan").innerHTML = headerText;
 
     //check if guessesRemaining is zero, if so, increase loss, alert player, and resetGame();
     if (guessesRemaining === 0) {
@@ -97,6 +106,13 @@ document.onkeyup = function (guess) {
         resetGame();
     }
 
-    document.getElementById("hangMan").innerHTML = headerText;
-    console.log(headerText);
+    //if there are no more blank lines left, increase win counter, alert player, and resetGame();
+    if(!wordLines.includes(" _ ")) {
+        wins++;
+        alert("Correct! The word is " + wordToGuess + ". You win, play again!");
+        resetGame();
+    }
+
+    
+
 }
